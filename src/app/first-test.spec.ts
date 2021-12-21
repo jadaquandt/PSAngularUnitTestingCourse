0163eef612e2d0649a82b8d86922bcbc47387448
Jada Quandt <jadaquandt@gmail.com>
2021-12-21 16:26:27 -0500

    Starting course again

--- src/app/first-test.spec.ts
+++ src/app/first-test.spec.ts
@@ -1,23 +1,24 @@
 describe('my first test', () => {
     //system under test = sut
     let sut;
-
     //This will reset the state so that we know that with every test we don't have any effects from a previous test that's holding over and polluting the state of future tests. 
     beforeEach(() => {
-        //This is very important. We need to put code inside of the beforeEach that resets the state so that we don't pollute future tests with changes that have been made in previous tests. 
+        //We need to put code inside of the beforeEach that resets the state so that we don't pollute future tests with changes that have been made in previous tests. 
+        //Reset the state
         sut = {}
     })
 
     //Next an actual test, which we use with an it function. It's very customary to start your it statements with the word should. 
-    //the describe and it functions should have strings that make sense when appended
+    //the functions should have strings that make sense when appended to the describe name
     it('should be true if true', () => {
-        //arrange
+        //arrange, set a new property called "a", set to false
         sut.a = false;
 
         //act 
         sut.a = true;
 
         //assert
+        //"toBe" is called a "matcher"
         expect(sut.a).toBe(true);
     })
 })
